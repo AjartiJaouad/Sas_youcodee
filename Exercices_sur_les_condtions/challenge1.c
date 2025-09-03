e4002328f04233814af2d842883e954106796b8c
ajartijaouad <ajartijaouad14@gmail.com>
2025-09-03 13:03:11 +0200

    test de parite

---
+++ Exercices_sur_les_condtions/challenge1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+int n ;
+printf("saisi un nombre  entier:");
+scanf("%d",&n);
+ if (n % 2 ==  0) 
+   printf("%d est nombre pair",n);
+else
+   printf("%d est nombree  impair",n);
+
+return 0; 
+}
