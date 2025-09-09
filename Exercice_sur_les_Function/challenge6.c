c3bd159f48325cbf535a56d6d3395832107fe474
ajartijaouad <ajartijaouad14@gmail.com>
2025-09-09 15:34:13 +0200

    Terme de fibonacci

---
+++ Exercice_sur_les_Function/challenge6.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+long long fibonacci(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+    int n;
+    printf("Entrez un entier positif : ");
+    scanf("%d", &n);
+
+    if (n < 0) {
+        printf("Veuillez entrer un entier positif.\n");
+    } else {
+        printf("Fibonacci(%d) = %lld\n", n, fibonacci(n));
+    }
+
+    return 0;
+}
